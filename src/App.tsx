9322634f9c6826088d5fb78048c8efa3bbd14e76
Pakkerman <Pakkermandev.gmail.com>
2023-02-27 16:26:50 +0800

    deploy to test on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ml5 from 'ml5'
 import './App.css'
 
 let audioContext = new AudioContext()
 let audioStream = await navigator.mediaDevices.getUserMedia({ audio: true })
-const pitch = ml5.pitchDetection('./crepe/', audioContext, audioStream, () => {
-  console.log('loaded')
-  getPitch()
-})
-let output
-
-function getPitch() {
-  pitch.getPitch((err: any, frequency: any) => {
-    if (err) console.log(err)
-    if (frequency) {
-      output = frequency
-      console.log(frequency)
-    } else {
-      console.log('No pitch detected')
-    }
-    setTimeout(() => getPitch(), 333)
-    // getPitch()
-  })
-}
+// const pitch = ml5.pitchDetection('./crepe/', audioContext, audioStream, () => {
+//   console.log('loaded')
+//   getPitch()
+// })
 
 window.addEventListener('click', async () => {
   await audioContext.resume()
@@ -30,13 +15,31 @@ window.addEventListener('click', async () => {
 })
 
 function App() {
-  // const [frequency, setFreqency] = useState(null)
+  const [frequency, setFrequency] = useState('waiting for frequency')
+  let pitch: any
 
-  return (
-    <div className="App">
-      <p>{frequency}</p>
-    </div>
-  )
+  useEffect(() => {
+    pitch = ml5.pitchDetection('./crepe/', audioContext, audioStream, () => {
+      console.log('loaded')
+      getPitch()
+    })
+  }, [])
+
+  function getPitch() {
+    pitch.getPitch((err: any, frequency: any) => {
+      if (err) console.log(err)
+      if (frequency) {
+        setFrequency(frequency)
+        console.log(frequency)
+      } else {
+        console.log('No pitch detected')
+      }
+      setTimeout(() => getPitch(), 333)
+      // getPitch()
+    })
+  }
+
+  return <div className="App">{frequency}</div>
 }
 
 export default App
